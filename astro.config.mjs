f64dc3c376a831252034fbe79c7228782a710d4d
Kamil M <116840531+proxima812@users.noreply.github.com>
2025-03-04 02:16:55 +0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import mdx from "@astrojs/mdx"
+import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
+import tailwindcss from "@tailwindcss/vite"
+import icon from "astro-icon"
+import metaTags from "astro-meta-tags"
+import { defineConfig } from "astro/config"
+
+export default defineConfig({
+	site: "https://example.com",
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	prefetch: {
+		defaultStrategy: "viewport",
+		prefetchAll: true,
+	},
+	integrations: [mdx(), sitemap(), icon(), metaTags(), react()],
+	output: "static",
+})
